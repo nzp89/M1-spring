ecfd2615eaee4288f1ed98f5a98803b996077e32
nzp89 <92073106+nzp89@users.noreply.github.com>
2024-04-25 10:47:17 +0900

    Update aeif_1neuron.c
    コメントアウトダサかったので<stdbool.h>入れてif(false)という力技を使う。まだ動けるver.

--- aeif_1neuron.c
+++ aeif_1neuron.c
@@ -5,21 +5,14 @@
 #include <math.h>
 #include "aeif_1neuron.h"
 
-#define TMAX 500.0 // time max [ms]
-#define DT 0.0001 // time difference [ms]
+#define TMAX 1000.0 // time max [ms]
+#define DT 0.001 // time difference [ms]
 
-double dvdt(double v_memb, double w_current, struct one_Neuron neuron){
-    // fprintf( stderr, "%f\n", exp( (v_memb - neuron.vt_theta)));
-
-    double exponent_term = (v_memb - neuron.vt_theta) / neuron.slopefactor;
-    if (exponent_term > 15) {
-        exponent_term = 15;  
-    }
-
-    return ( -neuron.gl_cond * (v_memb - neuron.el_rest) + neuron.gl_cond * neuron.slopefactor * exp( exponent_term ) + neuron.i_ext - w_current ) / neuron.c_capa;
+static inline double dvdt(double v_memb, double w_current, struct one_Neuron neuron){
+    return ( -neuron.gl_cond * (v_memb - neuron.el_rest) + neuron.gl_cond * neuron.slopefactor * exp( (v_memb - neuron.vt_theta) / neuron.slopefactor ) + neuron.i_ext - w_current ) / neuron.c_capa;
 }
 
-double dwdt(double v_memb, double w_current, struct one_Neuron neuron){
+static inline double dwdt(double v_memb, double w_current, struct one_Neuron neuron){
     return (neuron.a_cond * (v_memb - neuron.el_rest) - w_current) / neuron.tauw_cons;
 }
 
@@ -30,14 +23,13 @@ int main(){
     double v_memb, w_current;
     v_memb = neuron.el_rest;
     w_current = 0.0;
-// file setup
+
     FILE *v_file;
     char *v_filename = "output_txt/fig4a_voltage.txt";
     v_file = fopen(v_filename, "w");
     if(v_file == NULL){
         printf("cannot open the file : %s\n", v_filename);
     }
-
     int countmax = (int) TMAX / DT;
  
     for(int time = 0; time < countmax; time++){
@@ -46,34 +38,29 @@ int main(){
             fprintf(v_file, "%f %f\n", time * DT, v_memb);
         }
         
-        if(true){
-        // runge-kutta method
-            double kv1 = DT * dvdt(v_memb, w_current, neuron);
-            double kw1 = DT * dwdt(v_memb, w_current, neuron);
-
-            double kv2 = DT * dvdt(v_memb + 0.5 * kv1, w_current + 0.5 * kw1, neuron);
-            double kw2 = DT * dwdt(v_memb + 0.5 * kv1, w_current + 0.5 * kw1, neuron);
-
-            double kv3 = DT * dvdt(v_memb + 0.5 * kv2, w_current + 0.5 * kw2, neuron);
-            double kw3 = DT * dwdt(v_memb + 0.5 * kv2, w_current + 0.5 * kw2, neuron);
-
-            double kv4 = DT * dvdt(v_memb + kv3, w_current + kw3, neuron);
-            double kw4 = DT * dwdt(v_memb + kv3, w_current + kw3, neuron);
-
-            v_memb += (kv1 + 2 * kv2 + 2 * kv3 + kv4) / 6.0;
-            w_current += (kw1 + 2 * kw2 + 2 * kw3 + kw4) / 6.0;
+        if(false){
+             // runge-kutta method
+            double dvdt1 = dvdt(v_memb, w_current, neuron);
+            double dwdt1 = dwdt(v_memb, w_current, neuron);
+            double dvdt2 = dvdt(v_memb + 0.5 * DT * dvdt1, w_current + 0.5 * DT * dwdt1, neuron);
+            double dwdt2 = dwdt(v_memb + 0.5 * DT * dvdt1, w_current + 0.5 * DT * dwdt1, neuron);
+            double dvdt3 = dvdt(v_memb + 0.5 * DT * dvdt2, w_current + 0.5 * DT * dwdt2, neuron);
+            double dwdt3 = dwdt(v_memb + 0.5 * DT * dvdt2, w_current + 0.5 * DT * dwdt2, neuron);
+            double dvdt4 = dvdt(v_memb + DT * dvdt3, w_current + DT * dwdt3, neuron);
+            double dwdt4 = dwdt(v_memb + DT * dvdt3, w_current + DT * dwdt3, neuron);
+            v_memb += DT * (dvdt1 + 2 * dvdt2 + 2 * dvdt3 + dvdt4) / 6.0;
+            w_current += DT * (dwdt1 + 2 * dwdt2 + 2 * dwdt3 + dwdt4) / 6.0;
         }else{
-        // euler method
+            // euler method
             v_memb += DT * dvdt(v_memb, w_current, neuron);
-            w_current += DT * dwdt(v_memb, w_current, neuron);  
-        }
-        
+            w_current += DT * dwdt(v_memb, w_current, neuron);
+        } 
         if(v_memb > 0){
             fprintf(v_file, "%f %f\n", time * DT, 0.0);
             v_memb = neuron.vr_reset;
             w_current = w_current + neuron.b_reset;
         }
-        
+
     }
 
     fclose(v_file);
