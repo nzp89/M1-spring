fe8958f21092e4d6adab1740f321e94704293d80
nzp89 <92073106+nzp89@users.noreply.github.com>
2024-04-25 20:02:55 +0900

    Update aeif_1neuron.c
    デバッグ中。

--- aeif_1neuron.c
+++ aeif_1neuron.c
@@ -11,12 +11,15 @@
 double dvdt(double v_memb, double w_current, struct one_Neuron neuron, FILE *exp_file){
     double exp_term = (v_memb - neuron.vt_theta) / neuron.slopefactor;
     // exp_term = (exp_term > 15) ? 15: exp_term;
-    fprintf(exp_file, "%lf ", exp_term);
-    return ( -neuron.gl_cond * (v_memb - neuron.el_rest) + neuron.gl_cond * neuron.slopefactor * exp(exp_term) + neuron.i_ext - w_current ) / neuron.c_capa;
+    // double output = neuron.i_ext - w_current;
+    // fprintf(exp_file, "(%lf)-(%lf) %lf ",neuron.i_ext, w_current, output);
+    return (( -neuron.gl_cond * (v_memb - neuron.el_rest) + neuron.gl_cond * neuron.slopefactor * exp(exp_term) + neuron.i_ext - w_current ) / neuron.c_capa);
 }
 
-double dwdt(double v_memb, double w_current, struct one_Neuron neuron){
-    return (neuron.a_cond * (v_memb - neuron.el_rest) - w_current) / neuron.tauw_cons;
+double dwdt(double v_memb, double w_current, struct one_Neuron neuron, FILE *exp_file){
+    // double output = w_current;
+    // fprintf(exp_file, "%lf ", output);
+    return ((neuron.a_cond * (v_memb - neuron.el_rest) - w_current) / neuron.tauw_cons);
 }
 
 
@@ -29,7 +32,6 @@ int main(){
 // file setup
     FILE *v_file, *exp_file;
     char *v_filename = "output_txt/fig4a_voltage.txt";
-// debag menu (erase later)
     char *exp_filename = "output_txt/exp_nakami.txt";
     v_file = fopen(v_filename, "w");
     exp_file = fopen(exp_filename, "w");
@@ -42,34 +44,40 @@ int main(){
  
     for(int time = 0; time < countmax; time++){
         if(time % 1 == 0){
-            printf("time count : %d\n", time);
+            //printf("time count : %d\n", time);
             fprintf(v_file, "%f %f\n", time * DT, v_memb);
-            fprintf(exp_file, "%f ", time * DT);
+            fprintf(exp_file, "%f\n", time * DT);
         }
         
         if(true){
              // runge-kutta method
             double kv1 = DT * dvdt(v_memb, w_current, neuron, exp_file);
-            double kw1 = DT * dwdt(v_memb, w_current, neuron);
+            double kw1 = DT * dwdt(v_memb, w_current, neuron, exp_file);
+            fprintf(exp_file, "%lf = %f * {-%f * (%lf - %f) + %f * %f * exp(%lf) + %f - %lf} / %f\n", kv1, DT, neuron.gl_cond, v_memb, neuron.el_rest, neuron.gl_cond, neuron.slopefactor, (v_memb - neuron.vt_theta) / neuron.slopefactor, neuron.i_ext, w_current, neuron.c_capa);
 
             double kv2 = DT * dvdt(v_memb + 0.5 * kv1, w_current + 0.5 * kw1, neuron, exp_file);
-            double kw2 = DT * dwdt(v_memb + 0.5 * kv1, w_current + 0.5 * kw1, neuron);
-
+            double kw2 = DT * dwdt(v_memb + 0.5 * kv1, w_current + 0.5 * kw1, neuron, exp_file);
+            fprintf(exp_file, "%lf = %f * {-%f * (%lf - %f) + %f * %f * exp(%lf) + %f - %lf} / %f\n", kv2, DT, neuron.gl_cond, v_memb + 0.5 * kv1, neuron.el_rest, neuron.gl_cond, neuron.slopefactor, (v_memb + 0.5 * kv1 - neuron.vt_theta) / neuron.slopefactor, neuron.i_ext, w_current + 0.5 * kw1, neuron.c_capa);
+            
             double kv3 = DT * dvdt(v_memb + 0.5 * kv2, w_current + 0.5 * kw2, neuron, exp_file);
-            double kw3 = DT * dwdt(v_memb + 0.5 * kv2, w_current + 0.5 * kw2, neuron);
-
+            double kw3 = DT * dwdt(v_memb + 0.5 * kv2, w_current + 0.5 * kw2, neuron, exp_file);
+            fprintf(exp_file, "%lf = %f * {-%f * (%lf - %f) + %f * %f * exp(%lf) + %f - %lf} / %f\n", kv3, DT, neuron.gl_cond, v_memb + 0.5 * kv2, neuron.el_rest, neuron.gl_cond, neuron.slopefactor, (v_memb + 0.5 * kv2 - neuron.vt_theta) / neuron.slopefactor, neuron.i_ext, w_current + 0.5 * kw2, neuron.c_capa);
+            
             double kv4 = DT * dvdt(v_memb + kv3, w_current + kw3, neuron, exp_file);
-            double kw4 = DT * dwdt(v_memb + kv3, w_current + kw3, neuron);
+            double kw4 = DT * dwdt(v_memb + kv3, w_current + kw3, neuron, exp_file);
+            fprintf(exp_file, "%lf = %f * {-%f * (%lf - %f) + %f * %f * exp(%lf) + %f - %lf} / %f\n", kv4, DT, neuron.gl_cond, v_memb + kv3, neuron.el_rest, neuron.gl_cond, neuron.slopefactor, (v_memb + kv3 - neuron.vt_theta) / neuron.slopefactor, neuron.i_ext, w_current + kw3, neuron.c_capa);
 
             v_memb += (kv1 + 2 * kv2 + 2 * kv3 + kv4) / 6.0;
             w_current += (kw1 + 2 * kw2 + 2 * kw3 + kw4) / 6.0;
+            fprintf(exp_file, "%lf = v_old + (%lf + 2 * %lf + 2 * %lf + %lf) / 6\n", v_memb, kv1, kv2, kv3, kv4);
         }else{
             // euler method
             v_memb += DT * dvdt(v_memb, w_current, neuron, exp_file);
-            w_current += DT * dwdt(v_memb, w_current, neuron);
+            w_current += DT * dwdt(v_memb, w_current, neuron, exp_file);
+            fprintf(exp_file, "%lf = old_v + %f * {-%f * (%lf - %f) + %f * %f * exp(%lf) + %f - %lf} / %f\n",v_memb, DT, neuron.gl_cond, v_memb, neuron.el_rest, neuron.gl_cond, neuron.slopefactor, (v_memb - neuron.vt_theta) / neuron.slopefactor, neuron.i_ext, w_current, neuron.c_capa);
         } 
         fprintf(exp_file, "\n");
-        if(v_memb > 0){
+        if(v_memb > 0.0){
             fprintf(v_file, "%f %f\n", time * DT, 0.0);
             v_memb = neuron.vr_reset;
             w_current = w_current + neuron.b_reset;
